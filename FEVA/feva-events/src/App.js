78af70a20460d0b2529a79def9d3bcc8a2be53b4
kenhmaina <kenhmaina@gmail.com>
2023-11-22 23:18:03 +0300

    this is my first commit

---
+++ FEVA/feva-events/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
